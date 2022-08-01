54f935b72e0c311c9a8cfb0cd26c1adbd14d557f
AmbisProgramming <110365940+AmbisProgramming@users.noreply.github.com>
2022-08-01 12:54:59 +0700

    Add files via upload

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+let dataTinggiBadan = [152, 165, 172, 155, 163, 171, 180, 163, 168, 166];
+
+console.log("Perulangan Biasa");
+for (let i = 0; i < dataTinggiBadan.length; i++) {
+  console.log(dataTinggiBadan[i]);
+}
+console.log("Perulangan For of");
+for (let y of dataTinggiBadan) {
+  console.log(y);
+}
